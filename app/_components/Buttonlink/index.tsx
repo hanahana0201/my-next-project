69c128c33f9d50ec45dec4c9f7e935dcaa33a4a4
root <root@localhost.localdomain>
2025-03-15 13:26:02 +0000

    Fix button component

--- app/_components/Buttonlink/index.tsx
+++ app/_components/Buttonlink/index.tsx
@@ -5,7 +5,10 @@ type Props = {
 
 export default function Buttonlink({ href, children }: Props) {
   return (
-    <a href={href} className="block py-5 px-10 rounded-(--border-radius) w-75">
+    <a
+      href={href}
+      className="block py-5 px-10 rounded-(--border-radius) w-75 bg-[url(/arrow-right.svg)] bg-no-repeat bg-[right_20px_center]"
+    >
       {children}
     </a>
   );
