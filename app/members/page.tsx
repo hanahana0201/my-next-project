45bd6849e7807089a23102cfd2e515ea772f8be6
Masatoahi Hanai <hanahana0201@gmail.com>
2025-03-21 21:05:35 +0900

    メンバーコンポーネント作成（途中）

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,3 +1,43 @@
+import { url } from 'inspector';
+
+const data = {
+  contents: [
+    {
+      id: '1',
+      image: {
+        url: '/img-member1.jpg',
+        width: 240,
+        height: 240,
+      },
+      name: 'デイビッド・チャン',
+      position: 'CEO',
+      profile: 'プロフィールが入ります。',
+    },
+    {
+      id: '2',
+      image: {
+        url: '/img-member2.jpg',
+        width: 240,
+        height: 240,
+      },
+      name: 'デイビッド・チャン',
+      position: 'CCO',
+      profile: 'プロフィールが入ります。',
+    },
+    {
+      id: '3',
+      image: {
+        url: '/img-member3.jpg',
+        width: 240,
+        height: 240,
+      },
+      name: 'デイビッド・チャン',
+      position: 'CEO',
+      profile: 'プロフィールが入ります。',
+    },
+  ],
+};
+
 export default function Page() {
   return (
     <div className="m-5">
