0c111a3839935ef19b730e276d709cd210df0849
Masatoshi Hanai <hanahana0201@gmail.com>
2025-03-24 09:38:54 +0900

    ハンバーガーメニューの実装

--- app/_components/Menu/index.tsx
+++ app/_components/Menu/index.tsx
@@ -1,10 +1,16 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Menu() {
+  const [openMenu, setOpenMenu] = useState(false);
+  const handleMenuOpen = () => {
+    setOpenMenu(!openMenu);
+  };
   return (
     <div>
-      <nav className="max-sm:hidden">
+      <nav className={`max-sm:hidden ${openMenu ? "max-sm:block max-sm:fixed max-sm:top-0 max-sm:right-0 max-sm:bottom-0 max-sm:left-0 ma-sm:bg-bg-painted max-sm:text-text-unpainted max-sm:py-6 max-sm:px-4" : ""}`}>
         <ul className="flex text-white gap-10">
           <li>
             <Link href="/news">ニュース</Link>
@@ -17,7 +23,7 @@ export default function Menu() {
           </li>
         </ul>
       </nav>
-      <button className="hidden max-sm:flex max-sm:bg-none border-none cursor-pointer" >
+      <button className="hidden max-sm:flex max-sm:bg-none border-none cursor-pointer" onClick={handleMenuOpen} >
         <Image src="/menu.svg" alt=" メニュー" width={24} height={24} />
       </button>
     </div>
