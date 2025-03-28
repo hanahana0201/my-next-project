4ff0e509931ccd07678122779df2182fcd1faee0
Masatoshi Hanai <hanahana0201@gmail.com>
2025-03-29 01:47:47 +0900

    News詳細ページを作成

--- app/_components/Date/index.tsx
+++ app/_components/Date/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { formatDate } from "@/app/_libs/utils";
 
 type Props = {
   date: string;
@@ -8,7 +9,7 @@ export default function Date({ date }: Props) {
   return (
     <span className="flex items-center gap-2 mx-[0.8rem] my-0 text-base">
       <Image src="/clock.svg" alt="" width={16} height={16} priority />
-      {date}
+      {formatDate(date)}
     </span>
   );
 }
